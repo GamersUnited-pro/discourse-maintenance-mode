5b21edb10a95325c85490c467b91ff1c864a7714
GamersUnited-pro <cris.kolkman@gmail.com>
2025-08-21 22:55:42 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,59 +2,52 @@
 
 # name: discourse-maintenance-mode
 # about: Toggleable maintenance mode with stylish page + admin-only update notifications
-# version: 1.0.10
+# version: 1.0.11
 # authors: GamersUnited.pro
 # url: https://github.com/GamersUnited-pro/discourse-maintenance-plugin
 
 enabled_site_setting :maintenance_mode_enabled
 
-# -----------------------------
-# Plugin constants (top-level for jobs)
-# -----------------------------
-module ::DiscourseMaintenancePlugin
-  PLUGIN_NAME = "discourse-maintenance-plugin"
-  PLUGIN_VERSION = "1.0.10"
-  UPDATE_STORE_KEY = "last_notified_version"
-end
-
 after_initialize do
-  # Require controllers & jobs
-  require_dependency File.expand_path("app/controllers/maintenance_controller.rb", __dir__)
-  require_dependency File.expand_path("app/jobs/scheduled/check_maintenance_plugin_update.rb", __dir__)
-
-  # Routes
-  Discourse::Application.routes.append do
-    get "/maintenance" => "discourse_maintenance_plugin/maintenance#index"
+  module ::DiscourseMaintenancePlugin
+    PLUGIN_NAME = "discourse-maintenance-plugin"
+    PLUGIN_VERSION = "1.0.11"
+    UPDATE_STORE_KEY = "last_notified_version"
   end
 
-  # Maintenance check patch
-  ApplicationController.class_eval do
-    before_action :discourse_maintenance_check
+  require_dependency File.expand_path("../app/controllers/maintenance_controller.rb", __FILE__)
+  require_dependency File.expand_path("../app/jobs/scheduled/check_maintenance_plugin_update.rb", __FILE__)
 
-    private
+  Discourse::Application.routes.append do
+    get "/maintenance" => "maintenance#index"
+  end
 
-    def discourse_maintenance_check
-      return unless SiteSetting.maintenance_mode_enabled
-      return if current_user&.admin? || current_user&.moderator?
+  reloadable_patch do
+    ApplicationController.class_eval do
+      before_action :discourse_maintenance_check
 
-      allowed_prefixes = %w[
-        /maintenance /login /logout /session /users /u /user_activations /password /password_resets
-        /admin /assets /plugins /stylesheets /favicon
-        /letter_avatar_proxy /letter_avatar /humans.txt /robots.txt /manifest /service-worker
-      ]
+      private
 
-      path = request.path
-      return if allowed_prefixes.any? { |p| path.start_with?(p) }
+      def discourse_maintenance_check
+        return unless SiteSetting.maintenance_mode_enabled
+        return if current_user&.admin? || current_user&.moderator?
 
-      unless request.format.html?
-        render json: {
-          error: "maintenance_in_progress",
-          message: SiteSetting.maintenance_mode_message
-        }, status: 503
-        return
+        allowed_prefixes = %w[
+          /maintenance /login /logout /session /users /u /user_activations /password /password_resets
+          /admin /assets /plugins /stylesheets /favicon
+          /letter_avatar_proxy /letter_avatar /humans.txt /robots.txt /manifest /service-worker
+        ]
+
+        path = request.path
+        return if allowed_prefixes.any? { |p| path.start_with?(p) }
+
+        if request.format.html?
+          render template: "maintenance/index", layout: false, status: 503
+        else
+          # allow JSON/API internally; do not block
+          return
+        end
       end
-
-      render template: "maintenance/index", layout: false, status: 503
     end
   end
 end
