1efa95f96a35b4fd9302be2c3831e654c337591c
GamersUnited-pro <cris.kolkman@gmail.com>
2025-08-21 20:22:45 +0200

    Create maintenance_controller.rb

---
+++ config/app/controllers/maintenance_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class ::MaintenanceController < ::ApplicationController
+  skip_before_action :check_xhr
+  layout false
+
+  def index
+    # If maintenance is off, bounce home
+    unless SiteSetting.maintenance_mode_enabled
+      redirect_to "/" and return
+    end
+
+    @title = SiteSetting.maintenance_mode_title.presence || "We'll be back soon"
+    @message = SiteSetting.maintenance_mode_message.presence || "The forum is currently under maintenance. Please check back later."
+    @interval = (SiteSetting.maintenance_refresh_interval || 15).to_i
+  end
+end
