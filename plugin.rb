a0dbd261e7d51a8b96e211b5edfc7f16def93af2
GamersUnited-pro <cris.kolkman@gmail.com>
2025-08-21 14:55:15 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,11 +1,54 @@
 # plugins/discourse-maintenance-mode/plugin.rb
 # frozen_string_literal: true
 
+PLUGIN_NAME ||= "discourse-maintenance-mode".freeze
+PLUGIN_VERSION ||= "1.0.1".freeze
+
 enabled_site_setting :maintenance_mode_enabled
 
 after_initialize do
   require_dependency "application_controller"
+  require 'net/http'
+  require 'json'
 
+  # -----------------------------
+  # Module for release checking
+  # -----------------------------
+  module ::DiscourseMaintenanceMode
+    class << self
+      # Fetch latest release tag from GitHub
+      def latest_release
+        url = URI("https://api.github.com/repos/GamersUnited-pro/discourse-maintenance-mode/releases/latest")
+        res = Net::HTTP.get_response(url)
+        return nil unless res.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(res.body)
+        data["tag_name"]
+      rescue
+        nil
+      end
+
+      # Compare current plugin version to latest release
+      def update_available?
+        latest = latest_release
+        return false unless latest
+        Gem::Version.new(latest.gsub(/^v/, "")) > Gem::Version.new(PLUGIN_VERSION)
+      end
+    end
+  end
+
+  # -----------------------------
+  # Admin notification if update is available
+  # -----------------------------
+  if ::DiscourseMaintenanceMode.update_available?
+    AdminNotification.create!(
+      notification_type: AdminNotification.types[:custom],
+      message: "A new version of Discourse Maintenance Mode is available: #{::DiscourseMaintenanceMode.latest_release}. Please update!"
+    )
+  end
+
+  # -----------------------------
+  # Patch ApplicationController for maintenance mode
+  # -----------------------------
   class ::ApplicationController
     before_action :check_maintenance_mode
 
@@ -15,7 +58,7 @@ after_initialize do
       # Allow admins and moderators full access
       return if current_user && (current_user.admin? || current_user.moderator?)
 
-      # Allow login and registration pages even during maintenance
+      # Allow login, registration, and activation pages during maintenance
       allowed_paths = [
         "/login",
         "/logout",
@@ -24,11 +67,11 @@ after_initialize do
         "/user_activations",
         "/password_resets",
         "/site_settings",
-        "/notifications" # you can add more if needed
+        "/notifications"
       ]
       return if allowed_paths.any? { |path| request.path.start_with?(path) }
 
-      # Render maintenance message page
+      # Render maintenance page template
       render template: "discourse_maintenance_mode/maintenance", layout: false
     end
   end
