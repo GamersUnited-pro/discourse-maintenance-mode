71b1e58e66aeb89309a1615a2ef5bba84b509813
GamersUnited-pro <cris.kolkman@gmail.com>
2025-08-21 22:56:18 +0200

    Update maintenance_controller.rb

--- app/controllers/maintenance_controller.rb
+++ app/controllers/maintenance_controller.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-module ::DiscourseMaintenancePlugin
-  class MaintenanceController < ::ApplicationController
-    skip_before_action :check_xhr
-    layout false
+class ::MaintenanceController < ::ApplicationController
+  skip_before_action :check_xhr
+  layout false
 
-    def index
-      unless SiteSetting.maintenance_mode_enabled
-        redirect_to "/" and return
-      end
-
-      @title = SiteSetting.maintenance_mode_title.presence || "We'll be back soon"
-      @message = SiteSetting.maintenance_mode_message.presence || "The forum is currently under maintenance. Please check back later."
-      @interval = (SiteSetting.maintenance_refresh_interval || 15).to_i
+  def index
+    unless SiteSetting.maintenance_mode_enabled
+      redirect_to "/" and return
     end
+
+    @title = SiteSetting.maintenance_mode_title.presence || "We'll be back soon"
+    @message = SiteSetting.maintenance_mode_message.presence || "The forum is currently under maintenance. Please check back later."
+    @interval = (SiteSetting.maintenance_refresh_interval || 15).to_i
   end
 end
