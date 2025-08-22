3df6b1cb935e54de114512c802ad8df15af39a5c
GamersUnited-pro <cris.kolkman@gmail.com>
2025-08-22 22:36:13 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,15 +2,15 @@
 
 # name: discourse-maintenance-mode
 # about: Toggleable maintenance mode with stylish page + admin-only update notifications
-# version: 1.0.24
+# version: 1.0.30
 # authors: GamersUnited.pro
-# url: https://github.com/GamersUnited-pro/discourse-maintenance-plugin
+# url: https://github.com/GamersUnited-pro/discourse-maintenance-mode
 
 enabled_site_setting :maintenance_mode_enabled
 
 module ::DiscourseMaintenancePlugin
   PLUGIN_NAME = "discourse-maintenance-plugin"
-  PLUGIN_VERSION = "1.0.24"
+  PLUGIN_VERSION = "1.0.30"
   UPDATE_STORE_KEY = "last_notified_version"
 end
 
